c07f75bf32b9acab6f4d73f87756faa4fb461183
NiazbekovKuralbek <n.k.a.k.28241916@gmail.com>
2023-04-24 02:00:38 +0600

    void_commit

--- Tereksay/Employe.Designer.cs
+++ Tereksay/Employe.Designer.cs
@@ -42,7 +42,6 @@ namespace WindowsFormsApp2Tereksay
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(822, 289);
             this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Employe
             // 
