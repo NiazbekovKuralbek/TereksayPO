87ac1d6abe7402544ce0060b48a7c963bf3eeed3
NiazbekovKuralbek <n.k.a.k.28241916@gmail.com>
2023-04-25 17:33:48 +0600

    void_commit

--- Tereksay/DataBase.cs
+++ Tereksay/DataBase.cs
@@ -6,26 +6,28 @@ namespace WindowsFormsApp2Tereksay
     {
         private SqlConnection con =
                     new SqlConnection(@"Data Source=BEKZHAN\MURIM;Initial Catalog=Opt_mebel;Integrated Security=True");
-                //Server=BEKZHAN\MURIM;Database=Opt_mebel
-                public void openConnection()
-                {
-                    if (con.State == System.Data.ConnectionState.Closed)
-                    {
-                        con.Open();
-                    }
-                }
-        
-                public void closeConnection()
-                {
-                    if (con.State == System.Data.ConnectionState.Open)
-                    {
-                        con.Close();
-                    }
-                }
-        
-                public SqlConnection getConnection()
-                {
-                    return con;
-                }
+        //Server=BEKZHAN\MURIM;Database=Opt_mebel
+        //Server=ONIGIRI;Database=Opt_mebel
+
+        public void openConnection()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+
+        public SqlConnection getConnection()
+        {
+            return con;
+        }
     }
 }
